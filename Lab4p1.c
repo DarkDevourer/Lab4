c70ac7a42f18fa924a8440e938ccb791b3d9ad7c
DarkDevourer <kuhcrazan2000@mail.ru>
2021-04-18 13:03:32 +0300

    Release v1

--- Lab4p1.c
+++ Lab4p1.c
@@ -27,5 +27,6 @@ int main(int argc, char *argv[])
 			break;
 		}
 	}
+	close(rfile);
 	return 0;
 }
--- Lab4p2.c
+++ Lab4p2.c
@@ -5,10 +5,12 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
 int main(int argc, char *argv[])
 {
-	int data_processed;
+	int file_data;
+	int gamma_data;
 	int file_pipes[2];
 	int gamma_pipes[2];
 	char filebuf[BUFSIZ + 1];
@@ -20,7 +22,7 @@ int main(int argc, char *argv[])
 	memset(gammabuf, '\0', sizeof(gammabuf)); //TODO: ПРОВЕРИТЬ BUFSIZ
 	memset(tmpbuf, '\0', sizeof(tmpbuf)); //TODO: ПРОВЕРИТЬ BUFSIZ
 
-	if ((access(argv[1], F_OK) == -1) || (access(argv[2], F_OK) == -1))
+	if ((access(argv[1], F_OK) == -1) || (access(argv[2], F_OK) == -1) || (argc == 3))
 	{
 		printf("SOME FILES ARE NOT ACCESSEBLE!\n");
 		return -1;
@@ -50,8 +52,8 @@ int main(int argc, char *argv[])
 	{
 		int stat_val;
 		wait(&stat_val);
-		data_processed = read(file_pipes[0], filebuf, BUFSIZ);
-		printf("data_processed = %d\n", data_processed);
+		file_data = read(file_pipes[0], filebuf, BUFSIZ);
+		//printf("file_data = %d\n", file_data);
 	}
 
 	close(file_pipes[0]);
@@ -83,13 +85,42 @@ int main(int argc, char *argv[])
 	{
 		int stat_val;
 		wait(&stat_val);
-		data_processed = read(gamma_pipes[0], gammabuf, BUFSIZ);
-		printf("data_processed = %d\n", data_processed);
+		gamma_data = read(gamma_pipes[0], gammabuf, BUFSIZ);
+		//printf("gamma_data = %d\n", gamma_data);
 	}
 
-	printf("\n%s\n%s", filebuf,gammabuf);
+	//printf("\n%s\n%s", filebuf,gammabuf);
+	memset(tmpbuf, '\0', sizeof(tmpbuf)); //TODO: ПРОВЕРИТЬ BUFSIZ
 
 	close(gamma_pipes[0]);
 	close(gamma_pipes[1]);
+
+	if (gamma_data<file_data)
+	{
+			printf("FILE DATA IS BIGGER THEN GAMMA KEY. CONTINUE GAMMING?\nPRINT \"Y\" TO CONTINUE.\nPRINT ANYTHING ELSE TO QUIT.\n");
+			scanf("%s", tmpbuf);
+			if((toupper(tmpbuf[0])!='Y')||(tmpbuf[1]!='\0'))
+			{
+				printf("STOP GEMMING! QUIT PROGRAMM!\n");
+				return 0;
+			}
+	}
+	memset(tmpbuf, '\0', sizeof(tmpbuf)); //TODO: ПРОВЕРИТЬ BUFSIZ
+
+	int gammized_file = open(argv[3], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR| S_IWUSR);
+	if(gammized_file == -1)
+	{
+		printf("COULDN'T OPEN FILE FOR WRITING GAMMIZED DATA!\n");
+		return -10;
+	}
+
+	for(int i = 0;i < file_data; i++)
+	{
+		tmpbuf[i] = filebuf[i] ^ gammabuf[i%gamma_data];
+	}
+	write(gammized_file, &tmpbuf, file_data);
+	close(gammized_file);
+	printf("GAMMING COMPLETED SUCCESSFULLY!\n");
+
 	return 0;
 }
